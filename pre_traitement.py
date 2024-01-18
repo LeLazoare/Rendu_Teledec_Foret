4372abb198a19d6e24df04ea65bad64ddbde385c
Tom M <151040049+Koadao@users.noreply.github.com>
2024-01-18 16:38:41 +0100

    merged ndvi update

--- pre_traitement.py
+++ pre_traitement.py
@@ -9,7 +9,7 @@ Authors: M.Tarby - G.Ruiz - L.Sauger - T.Mervant || SIGMA 2024
 
 #lib import
 import sys
-import os
+import os, shutil
 import glob
 import time
 
@@ -20,7 +20,7 @@ folder = 'C:/Users/Xenerios/Desktop/adv_remote-sensing/forest_class'
 ##preprocess main hub
 ####################################
 #convert format
-filepaths = glob.glob(os.path.join(folder,'data_ori/S2A_15cc_FRE/S2A_L2A_FRE_ALL/24-02-2021/*.tif'))
+filepaths = glob.glob(os.path.join(folder,'data_ori/S2A_15cc_FRE/S2A_L2A_FRE_ALL/16-12-2021/*.tif'))#FILL IN DATE
 
 output_dir = os.path.join(folder, 'res/convert-1')
 #os_makedirs(output_dir, exist_ok=True) - if output_dir does not exist
@@ -67,6 +67,11 @@ for band_file in filepaths:
 end = time.time()
 print('processed in:', end - start)
 
+#WARNING: wait for process to end before removing files
+#remove files from previous process -- convert
+for f in filepaths:
+    os.remove(f)
+
 ####################################
 #mask || same clipping method should be used
 #ensure same extent (condition)
@@ -100,6 +105,12 @@ for band_file in filepaths:
 end = time.time()
 print('processed in:', end - start)
 
+#WARNING: wait for process to end before removing files
+#remove files from previous process -- warp
+for f in filepaths:
+    os.remove(f)
+
+
 ####################################
 #build multispectral img - single date only
 filepaths = glob.glob(os.path.join(folder,'res/mask-3/*.tif'))
@@ -108,23 +119,67 @@ start = time.time()
 
 ms = preprocess.build_ms(
     filepaths,
-    'C:/Users/Xenerios/Desktop/adv_remote-sensing/forest_class/res/multispectral/24-02-2021.tif'#fill in date
+    False,
+    'C:/Users/Xenerios/Desktop/adv_remote-sensing/forest_class/res/multispectral/2021-12-16.tif'#FILL IN DATE
 )
 
 end = time.time()
 print('processed in:', end - start)
- 
+
+#WARNING: wait for process to end before removing files
+#remove files from previous process -- mask
+for f in filepaths:
+    os.remove(f)
 
 ####################################
 #build multispectral img - multiple dates
 
-#not tested - merging raster files already containing multiple bands
 filepaths = glob.glob(os.path.join(folder,'res/multispectral/*.tif'))
 start = time.time()
 
 ms = preprocess.build_ms(
     filepaths,
+    True,
     'C:/Users/Xenerios/Desktop/adv_remote-sensing/forest_class/res/multispectral/maj_ms/maj_ms.tif'
+)
+
+end = time.time()
+print('processed in:', end - start)
+
+####################################
+#compute NDVI for each date
+filepaths = glob.glob(os.path.join(folder,'res/multispectral/*.tif'))
+
+output_dir = os.path.join(folder, 'res/ndvi')
+#os_makedirs(output_dir, exist_ok=True) - if output_dir does not exist
+
+start = time.time()
+for ms_file in filepaths:
+
+    #generate unique output_fpath
+    file_name, file_extension = os.path.splitext(os.path.basename(ms_file))
+    output_file = os.path.join(output_dir, f'{file_name}_ndvi{file_extension}')
+
+    #compute NDVI
+    ndvi = preprocess.compute_ndvi(
+        ms_file,
+        3,
+        7,
+        output_file
+    )
+
+end = time.time()
+print('processed in:', end - start)
+
+####################################
+#merge NDVI into a single raster
+filepaths = glob.glob(os.path.join(folder,'res/ndvi/*.tif'))
+start = time.time()
+
+maj_ndvi = preprocess.build_ms(
+    filepaths,
+    False,
+    'C:/Users/Xenerios/Desktop/adv_remote-sensing/forest_class/res/ndvi/maj_ndvi/maj_ndvi.tif'
 )
 
 end = time.time()
