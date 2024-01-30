725e1c8be4caa618d831da8801183c295a362af4
Tom M <151040049+Koadao@users.noreply.github.com>
2024-01-30 17:10:09 +0100

    Add files via upload

--- pre_traitement.py
+++ pre_traitement.py
@@ -4,26 +4,61 @@ preprocess -> base img and ndvi builder
 
 Requirements: my_function.py script file | librairies as follows (sys, os, glob)
 
-Authors: M.Tarby - G.Ruiz - L.Sauger - T.Mervant || SIGMA 2024
+Authors: M.Tarby - T.Mervant || SIGMA 2024
 """
 
 #lib import
 import sys
-import os, shutil
+import os
 import glob
 import time
 
 sys.path.append('C:/Users/Xenerios/Desktop/adv_remote-sensing/forest_class/scripts_')
 import my_function as preprocess
 
-folder = 'C:/Users/Xenerios/Desktop/adv_remote-sensing/forest_class'
+#specify existing folder
+folder = 'C:/Users/Xenerios/Desktop/adv_remote-sensing/tst'
+
+#required base tree structure
+inputs_folder = os.path.join(folder, 'inputs')
+if not os.path.exists(inputs_folder):
+        os.makedirs(inputs_folder)
+        print(f"Created folder: {inputs_folder}")        
+else:
+        print(f"Folder already exists: {inputs_folder}")
+
+res_folder = os.path.join(folder, 'res')
+if not os.path.exists(res_folder):
+        os.makedirs(res_folder)
+        print(f"Created folder: {res_folder}")
+else:
+        print(f"Folder already exists: {res_folder}")
+
+input_subfolders = ['sat_img']
+for subfolder in input_subfolders:
+        subfolder_path = os.path.join(inputs_folder, subfolder)
+        if not os.path.exists(subfolder_path):
+            os.makedirs(subfolder_path)
+            print(f"Created folder: {subfolder_path}")
+        else:
+            print(f"Folder already exists: {subfolder_path}")
+
+res_subfolders = ['convert-1', 'warp-2', 'mask-3', 'multispectral', 'ndvi']
+for subfolder in res_subfolders:
+        subfolder_path = os.path.join(res_folder, subfolder)
+        if not os.path.exists(subfolder_path):
+            os.makedirs(subfolder_path)
+            print(f"Created folder: {subfolder_path}")
+        else:
+            print(f"Folder already exists: {subfolder_path}")
+
+
 ##preprocess main hub
 ####################################
 #convert format
-filepaths = glob.glob(os.path.join(folder,'data_ori/S2A_15cc_FRE/S2A_L2A_FRE_ALL/16-12-2021/*.tif'))#FILL IN DATE
+filepaths = glob.glob(os.path.join(folder,'inputs/sat_img/19-07-2021/*.tif'))#FILL IN DATE
 
 output_dir = os.path.join(folder, 'res/convert-1')
-#os_makedirs(output_dir, exist_ok=True) - if output_dir does not exist
 
 start = time.time()
 for band_file in filepaths:
@@ -44,9 +79,9 @@ print('processed in:', end - start)
 ####################################
 #reproj - up/down sample - clip
 filepaths = glob.glob(os.path.join(folder,'res/convert-1/*.tif'))
+roi_path = os.path.join(folder, 'inputs/emprise_etude.shp')
 
 output_dir = os.path.join(folder, 'res/warp-2')
-#os_makedirs(output_dir, exist_ok=True) - if output_dir does not exist
 
 start = time.time()
 for band_file in filepaths:
@@ -59,7 +94,7 @@ for band_file in filepaths:
     warped = preprocess.warp_tif(
         band_file,
         output_file,
-        'C:/Users/Xenerios/Desktop/adv_remote-sensing/forest_class/data_ori/emprise_etude.shp',
+        roi_path,
         'EPSG:2154',
         (10, 10)
     )
@@ -76,9 +111,9 @@ for f in filepaths:
 #mask || same clipping method should be used
 #ensure same extent (condition)
 filepaths = glob.glob(os.path.join(folder,'res/warp-2/*.tif'))
-mask_fpath = 'C:/Users/Xenerios/Desktop/adv_remote-sensing/forest_class/res/masque_foret.tif'
+mask_fpath = os.path.join(folder, 'inputs/masque_foret.tif')
 output_dir = os.path.join(folder, 'res/mask-3')
-#os_makedirs(output_dir, exist_ok=True) - if output_dir does not exist
+
 
 start = time.time()
 for band_file in filepaths:
@@ -110,17 +145,16 @@ print('processed in:', end - start)
 for f in filepaths:
     os.remove(f)
 
-
 ####################################
 #build multispectral img - single date only
 filepaths = glob.glob(os.path.join(folder,'res/mask-3/*.tif'))
-
+msdate_path = os.path.join(folder, 'res/multispectral/2021-07-19.tif')#FILL IN DATE ACCORDINGLY
 start = time.time()
 
 ms = preprocess.build_ms(
     filepaths,
     False,
-    'C:/Users/Xenerios/Desktop/adv_remote-sensing/forest_class/res/multispectral/2021-12-16.tif'#FILL IN DATE
+    msdate_path
 )
 
 end = time.time()
@@ -133,6 +167,12 @@ for f in filepaths:
 
 ####################################
 #build multispectral img - multiple dates
+output_dir = os.path.join(folder, 'res/multispectral/maj_ms/Serie_temp_S2_allbands.tif')
+#extract the directory path from the file path
+output_dir_path = os.path.dirname(output_dir)
+#check if the directory exists, and create it if not
+if not os.path.exists(output_dir_path):
+    os.makedirs(output_dir_path)
 
 filepaths = glob.glob(os.path.join(folder,'res/multispectral/*.tif'))
 start = time.time()
@@ -140,7 +180,7 @@ start = time.time()
 ms = preprocess.build_ms(
     filepaths,
     True,
-    'C:/Users/Xenerios/Desktop/adv_remote-sensing/forest_class/res/multispectral/maj_ms/maj_ms.tif'
+    output_dir
 )
 
 end = time.time()
@@ -151,7 +191,11 @@ print('processed in:', end - start)
 filepaths = glob.glob(os.path.join(folder,'res/multispectral/*.tif'))
 
 output_dir = os.path.join(folder, 'res/ndvi')
-#os_makedirs(output_dir, exist_ok=True) - if output_dir does not exist
+#extract the directory path from the file path
+output_dir_path = os.path.dirname(output_dir)
+#check if the directory exists, and create it if not
+if not os.path.exists(output_dir_path):
+    os.makedirs(output_dir_path)
 
 start = time.time()
 for ms_file in filepaths:
@@ -176,10 +220,17 @@ print('processed in:', end - start)
 filepaths = glob.glob(os.path.join(folder,'res/ndvi/*.tif'))
 start = time.time()
 
+output_dir = os.path.join(folder, 'res/ndvi/maj_ndvi/Serie_temp_S2_ndvi.tif')
+#extract the directory path from the file path
+output_dir_path = os.path.dirname(output_dir)
+#check if the directory exists, and create it if not
+if not os.path.exists(output_dir_path):
+    os.makedirs(output_dir_path)
+
 maj_ndvi = preprocess.build_ms(
     filepaths,
     False,
-    'C:/Users/Xenerios/Desktop/adv_remote-sensing/forest_class/res/ndvi/maj_ndvi/maj_ndvi.tif'
+    output_dir
 )
 
 end = time.time()
